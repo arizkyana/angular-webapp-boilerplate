6ec7b7f35f5058cbcca78596bc29396ea923c671
arizkyana <agung.rizkyana@mail.unpas.ac.id>
2018-01-11 07:17:46 +0700

    Coba terapkan selected
    - belum berhasil

--- controllers/setting/menu/update.js
+++ controllers/setting/menu/update.js
@@ -6,52 +6,73 @@ angular.module('foodgasm')
         'API',
         'Menu',
         '$stateParams',
-        function($rootScope, $scope, $resource, API, Menu, $stateParams) {
+        function ($rootScope, $scope, $resource, API, Menu, $stateParams) {
 
             $scope.menu = {};
 
-            Menu.get({ id: $stateParams.id })
+            Menu.get({id: $stateParams.id})
                 .$promise
-                .then(function(result) {
+                .then(function (result) {
                     $scope.menu = result;
                 })
-                .catch(function(err) {
+                .catch(function (err) {
                     console.log(err);
                 });
 
-            $scope.save = function() {
+            $scope.save = function () {
                 $scope.menu.parent = Number($scope.menu.parent);
                 Menu.create($scope.menu)
                     .$promise
-                    .then(function(result) {
+                    .then(function (result) {
                         console.log(result);
                     })
-                    .catch(function(err) {
+                    .catch(function (err) {
                         console.log(err);
                     });
             };
 
         }
     ])
-    .directive('parents', ['$ocLazyLoad', '$resource', 'API', '$q', '$compile', function($ocLazyLoad, $resource, API, $q, $compile) {
+    .directive('parents', [
+        '$ocLazyLoad',
+        '$resource',
+        'API',
+        '$q',
+        '$stateParams',
+        'Menu',
+        '$timeout',
+        '$compile',
+        function ($ocLazyLoad, $resource, API, $q, $stateParams, Menu, $timeout, $compile) {
 
-        return {
-            restrict: 'A',
-            link: function(scope, element, attrs) {
-                $ocLazyLoad.load('themes/js/plugins/select2/select2.full.min.js')
-                    .then(function() {
-                        return $resource(API.BASE_URI + '/menu').query().$promise
-                    })
-                    .then(function(result) {
-                        scope.parents = result;
-                        element.select2();
-                    })
-                    .catch(function(err) {
-                        console.log(err);
-                    });
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    $ocLazyLoad.load('themes/js/plugins/select2/select2.full.min.js')
+                        .then(function () {
+                            return $q.all([
+                                Menu.query().$promise,
+                                Menu.get({id: $stateParams.id}).$promise
+                            ])
+                        })
+                        .then(function (results) {
+
+                            scope.parents = results[0];
+                            scope.menu.parent = 9;
+                            // scope.parent = results[1].parent;
+                            $compile(element.select2())(scope);
+                            $timeout(function(){
+                                console.log(results[1].parent);
+                                // element.select2().val(results[1].parent).trigger('change');
+                            }, 1000, false);
+
+
+                        })
+                        .catch(function (err) {
+                            console.log(err);
+                        });
+                }
+
+
             }
 
-
-        }
-
-    }]);
+        }]);
